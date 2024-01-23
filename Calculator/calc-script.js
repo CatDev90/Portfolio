a83c334bcfd9a682914c4f8ab33bd5ffa0ac6bac
CatDev90 <c.dev@hotmail.com>
2024-01-23 17:04:47 +0000

    first commit Portfolio

---
+++ Calculator/calc-script.js
@@ -0,0 +1,21 @@
+// Get the display element
+var display = document.getElementById('display');
+
+// Function to append to the display
+function appendToDisplay(value){
+    display.value += value;
+}
+
+// Function to clear the display
+function clearDisplay(){
+    display.value = "";
+}
+
+// Function to calculate the result
+function calculate(){
+    try {
+        display.value = eval(display.value);
+    } catch(e) {
+        display.value = "Error";
+    }
+}
