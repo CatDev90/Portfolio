bad22bfcc34f4cba4602640452950cd2546a6ca4
CatDev90 <c.dev@hotmail.com>
2024-01-24 21:08:46 +0000

    Changes made to portfolio Changes made to portfolio

--- chatApp/server.js
+++ chatApp/server.js
@@ -1,35 +1,30 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+
 const app = express();
 const server = http.createServer(app);
-
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({ server });
 
 app.use(express.static(__dirname + '/public'));
+
 wss.on('connection', (ws) => {
+    console.log('A new client connected');
 
-console.log('A new client connected');
+    ws.on('message', (message) => {
+        console.log(`Received: ${message}`);
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
 
-ws.on('message', (message) =>{
-console.log(`Received: ${message}`);
-wss.clients.forEach((client) => {
-if(client.readyState === WebSocket.OPEN){
-client.send(message);
-}
-
-});
-
-});
-
-ws.on('close', () => {
-console.log('A client disconnection');
-
-});
-
+    ws.on('close', () => {
+        console.log('A client disconnection');
+    });
 });
 
 server.listen(3000, () => {
-console.log('Server started on http://localhost:3000');
-
-})
+    console.log('Server started on http://localhost:3000');
+});
